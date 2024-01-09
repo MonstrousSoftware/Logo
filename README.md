6a47216f973007f321979db8b22fa2973160ee6f
Monstrous <49096535+MonstrousSoftware@users.noreply.github.com>
2024-01-09 21:10:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Logo
+
+A [libGDX](https://libgdx.com/) project generated with [gdx-liftoff](https://github.com/tommyettinger/gdx-liftoff).
+
+Displaying a 3d LibGDX logo using gdx-gtlf.
+
+Note:
+- the camera motion is sickening, I know...
+- the backdrop object should not move with the logo. It is just there for some contrast.
+- this could be the basis to do some cute animation with the logo, maybe as game intro.
+
+![screen shot](screenshot.png)
---
+++ core/src/main/java/com/monstrous/logo/Main.java
@@ -0,0 +1,128 @@
+package com.monstrous.logo;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector3;
+import net.mgsx.gltf.loaders.gltf.GLTFLoader;
+import net.mgsx.gltf.scene3d.attributes.PBRCubemapAttribute;
+import net.mgsx.gltf.scene3d.attributes.PBRFloatAttribute;
+import net.mgsx.gltf.scene3d.attributes.PBRTextureAttribute;
+import net.mgsx.gltf.scene3d.lights.DirectionalLightEx;
+import net.mgsx.gltf.scene3d.lights.DirectionalShadowLight;
+import net.mgsx.gltf.scene3d.scene.Scene;
+import net.mgsx.gltf.scene3d.scene.SceneAsset;
+import net.mgsx.gltf.scene3d.scene.SceneManager;
+import net.mgsx.gltf.scene3d.scene.SceneSkybox;
+import net.mgsx.gltf.scene3d.utils.IBLBuilder;
+
+public class Main extends ApplicationAdapter
+{
+    private static final String FILE_NAME = "models/libGDX-logo.gltf";
+	private static final int SHADOW_MAP_SIZE = 2048;
+
+	private SceneManager sceneManager;
+	private SceneAsset sceneAsset;
+	private Scene scene;
+	private PerspectiveCamera camera;
+	private Cubemap diffuseCubemap;
+	private Cubemap environmentCubemap;
+	private Cubemap specularCubemap;
+	private Texture brdfLUT;
+	private float time;
+	private SceneSkybox skybox;
+	private DirectionalLightEx light;
+	private CameraInputController camController;
+
+
+	@Override
+	public void create() {
+
+		// create scene
+		sceneAsset = new GLTFLoader().load(Gdx.files.internal(FILE_NAME ));
+		scene = new Scene(sceneAsset.scene);
+		sceneManager = new SceneManager();
+		sceneManager.addScene(scene);
+
+		// setup camera (The BoomBox model is very small so you may need to adapt camera settings for your scene)
+		camera = new PerspectiveCamera(60f, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		float d = 250f;
+		camera.near = 0.01f;
+		camera.far = 1000f;
+		sceneManager.setCamera(camera);
+
+		// input multiplexer to input to GUI and to cam controller
+		InputMultiplexer im = new InputMultiplexer();
+		Gdx.input.setInputProcessor(im);
+		camController = new CameraInputController(camera);
+		im.addProcessor(camController);
+
+		sceneManager.environment.set(new PBRFloatAttribute(PBRFloatAttribute.ShadowBias, 0.001f));
+
+		// setup light
+		//light = new DirectionalLightEx();
+		light = new DirectionalShadowLight(SHADOW_MAP_SIZE, SHADOW_MAP_SIZE).setViewport(5,5,5,40);
+
+		light.direction.set(1, -3, 1).nor();
+		light.color.set(Color.WHITE);
+		sceneManager.environment.add(light);
+
+		// setup quick IBL (image based lighting)
+		IBLBuilder iblBuilder = IBLBuilder.createOutdoor(light);
+		environmentCubemap = iblBuilder.buildEnvMap(1024);
+		diffuseCubemap = iblBuilder.buildIrradianceMap(256);
+		specularCubemap = iblBuilder.buildRadianceMap(10);
+		iblBuilder.dispose();
+
+		// This texture is provided by the library, no need to have it in your assets.
+		brdfLUT = new Texture(Gdx.files.classpath("net/mgsx/gltf/shaders/brdfLUT.png"));
+
+		sceneManager.setAmbientLight(0.1f);
+		sceneManager.environment.set(new PBRTextureAttribute(PBRTextureAttribute.BRDFLUTTexture, brdfLUT));
+		sceneManager.environment.set(PBRCubemapAttribute.createSpecularEnv(specularCubemap));
+		sceneManager.environment.set(PBRCubemapAttribute.createDiffuseEnv(diffuseCubemap));
+
+		// setup skybox
+		skybox = new SceneSkybox(environmentCubemap);
+		sceneManager.setSkyBox(skybox);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		sceneManager.updateViewport(width, height);
+	}
+
+	@Override
+	public void render() {
+		float deltaTime = Gdx.graphics.getDeltaTime();
+		time += deltaTime;
+
+        float t = MathUtils.PI * Math.abs(MathUtils.sin(time/5f));
+
+		// animate camera
+		camera.position.setFromSpherical(t*MathUtils.PI/4, .3f).scl(7f);
+		camera.up.set(Vector3.Y);
+		camera.lookAt(Vector3.Zero);
+		camera.update();
+		camController.update();
+
+		// render
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+		sceneManager.update(deltaTime);
+		sceneManager.render();
+	}
+
+	@Override
+	public void dispose() {
+		sceneManager.dispose();
+		sceneAsset.dispose();
+		environmentCubemap.dispose();
+		diffuseCubemap.dispose();
+		specularCubemap.dispose();
+		brdfLUT.dispose();
+		skybox.dispose();
+	}
+}
